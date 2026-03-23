66acd0a437c8997b5172d2068f8e2623ff4ccf62
fennerys <fennerysseah@gmail.com>
2026-03-23 07:56:34 +0000

    test

--- Actions.java
+++
@@ -1,7 +0,0 @@
-public interface Actions 
-{
-    abstract void basic_attack();
-    abstract void special_attack();
-    abstract void defend();
-    abstract void use_item();
-}
--- Player.java
+++ Player.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
+import java.util.List;
 
 public abstract class Player extends Combatant
 {
-    private item[] items;
-    private int special_cooldown;
+    private Item[] items;
+    private int specialCooldown;
     private static final int MAX_ITEMS = 10;
     private List<Enemy> enemies;
+
+    Scanner scan = new Scanner(System.in);
 
     public Player(String name, int maxHP, int HP, int ATK, int DEF, int SPD)
     {
         super(name, maxHP, HP, ATK, DEF, SPD);
-        //capping the number of items a player can hold to 10
-        items = new item[MAX_ITEMS];
-        special_cooldown = 0;
+
+        // capping the number of items a player can hold to 10
+        items = new Item[MAX_ITEMS];
+        specialCooldown = 0;
     }
 
     public void chooseAction()
     {
-        //printing relevent UI
-        Scanner scan = new Scanner(System.in);
+        // printing relevant UI
         System.out.println("What action would you like to perform: ");
         System.out.println("---------------------------------------");
         System.out.println("1) Basic Attack");
@@ -26,46 +29,33 @@ public abstract class Player extends Combatant
         System.out.println("3) Defend");
         System.out.println("4) Item");
         System.out.println("---------------------------------------");
-        System.out.println("Please input a number 1-4: ");
+        System.out.print("Please input a number 1-4: ");
 
-        //getting user input
-        int user_input = scan.nextInt();
+        // getting user input
+        int userInput = scan.nextInt();
 
-        //activating the correct methods
-        switch (user_input)
+        // activating the correct methods
+        switch (userInput)
         {
             case 1:
-            {
-                this.basic_attack();
+                this.basicAttack();
                 break;
-            }
+
             case 2:
-            {
-                this.special_attack();
+                this.specialAttack();
                 break;
-            }
+
             case 3:
-            {
                 this.defend();
                 break;
-            }
+
             case 4:
-            {
-                //not too sure how to use an item
-                this.use_item();
+                this.useItem();
                 break;
-            }
+
+            default:
+                System.out.println("Invalid input.");
+                break;
         }
     }
-
-    //methods to be implemented by wizard and warrior class later
-    abstract void special_attack();
-    abstract void basic_attack(List<Enemy> enemies);
-
-    public void defend()
-    {
-        this.DEF += 10;
-        //idk how to make it last for only 2 turns
-    }
-
 }
