2d936925d9f5cff091ff12290fb05451c06a4822
fennerys <fennerysseah@gmail.com>
2026-03-23 05:35:16 +0000

    implementation of the player class

--- Player.java
+++ Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public abstract class Player extends Combatant
 {
     private List<item> items;
@@ -5,6 +7,50 @@ public abstract class Player extends Combatant
 
     public void chooseAction()
     {
-        
+        //printing relevent UI
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What action would you like to perform: ");
+        System.out.println("---------------------------------------");
+        System.out.println("1) Basic Attack");
+        System.out.println("2) Special Attack");
+        System.out.println("3) Defend");
+        System.out.println("4) Item");
+        System.out.println("---------------------------------------");
+        System.out.println("Please input a number 1-4: ");
+
+        //getting user input
+        int userinput = scan.nextInt();
+
+        //activating the correct methods
+        switch (userinput)
+        {
+            case 1:
+            {
+                this.basicAttack();
+            }
+            case 2:
+            {
+                this.specialAttack();
+            }
+            case 3:
+            {
+                this.defend();
+            }
+            case 4:
+            {
+                this.useItem();
+            }
+        }
     }
+
+    //methods to be implemented by wizard and warrior class later
+    abstract void specialAttack();
+    abstract void basicAttack();
+
+    public void defend()
+    {
+        this.DEF += 10;
+        //idk how to make it last for only 2 turns
+    }
+
 }
